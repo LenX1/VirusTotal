910c4d14e2502c8964828d283b8f46304ac595cb
LenX1 <lennart.chario@outlook.de>
2025-10-16 12:20:30 +0200

    Update VirusTotal_File_Uploader_Dir.py
    response saved to csv

--- VirusTotal_File_Uploader_Dir.py
+++ VirusTotal_File_Uploader_Dir.py
@@ -1,17 +1,25 @@
 
 import requests
 import os
+import datetime
+
 
 Api_Key = "InsertApiKeyHere"
 
 file_type = "multipart/form-data"
 url = "https://www.virustotal.com/api/v3/files"
 
-for file in (os.listdir()[:]):
+timestamp_csv = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+
+for file in (os.listdir()[:]):  # [Start:End]
     files = { "file": (f"{file}", open(f"{file}", "rb"), f"{file_type}") }
     headers = {
         "accept": "application/json",
         "x-apikey": f"{Api_Key}",
     }
     response = requests.post(url, files=files, headers=headers)
-    print(response.text)
+
+    with open(f'VirusTotal-Upload_{timestamp_csv}.csv', 'a') as f:
+        f.write(f'{response.text}\n')
+
+
